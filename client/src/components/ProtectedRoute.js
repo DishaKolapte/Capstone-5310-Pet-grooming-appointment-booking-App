baeb4107993f5f2a6949dd994f2795d5d4cd7a9b
DishaKolapte <dishakolapate@gmail.com>
2024-12-11 03:30:02 -0500

    updated route

--- client/src/components/ProtectedRoute.js
+++ client/src/components/ProtectedRoute.js
@@ -26,10 +26,7 @@ export default function ProtectedRoute({ children }) {
       dispatch(hideLoading());
       if (res.data.success) {
         dispatch(setUser(res.data.data));
-      } else {
-        localStorage.clear();
-        <Navigate to="/login" />;
-      }
+      } 
     } catch (error) {
       localStorage.clear();
       dispatch(hideLoading());
